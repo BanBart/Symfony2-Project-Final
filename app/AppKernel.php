3e0885b14a33a5a474a9153fc4f015d5b9f17258
SakyHank <sakyhank@gmail.com>
2014-12-07 16:19:15 +0100

    delete bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Project\LineOfWorkBundle\ProjectLineOfWorkBundle(),
-            new SakyHank\OfficialSiteBundle\SakyHankOfficialSiteBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Sonata\CoreBundle\SonataCoreBundle(),
--- src/SakyHank/OfficialSiteBundle/SakyHankOfficialSiteBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SakyHank\OfficialSiteBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SakyHankOfficialSiteBundle extends Bundle
-{
-}
