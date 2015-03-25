ddf3080165eb9b75c362296a6263e0f81bd591f2
BanBart <banasik.bart@gmail.com>
2015-03-25 22:42:19 +0100

    Update readme file, add new fixtures for jobs

--- README.md
+++ README.md
@@ -10,14 +10,39 @@ It's not a finished one, and will probably never be finished.
 
 ================================================================================
 You can check how this "site" looks at www.sakyhank.vot.pl
- 
-    -The search wont work, because i have no idea how to run Elastica Search
-    Engine on remote server, all my tries had failed ;D
+================================================================================
 
-    -There may occur some problems with mails.
+Project contain:
 
-    -you can log-in as user1 with password: pass1. Creation of acc may not work
-    because of google and their privacy policy.
+->Register/login with email confirmation, also password change. I used
+FOSUserBundle
 
-    -be aware of bugs in responsive layout desing, i'll update it in future, 
-    maybe:). 
+->Login via AJAX technology if javascript is enabled.
+
+->Internationalization currently only en and a few words in polish(check navbar,
+when u switch to pl language to notice a change ;)
+
+->A little javascript, u can found it at slogan, it should animate with 10s
+latency.
+
+->Admin panel which was implemented thanks to SonataAdminBundle.
+
+->Commercial also is changing over time(10s), every request via AJAX,a new 
+record(random one) from database is fetched and is replaced in html.
+
+...
+
+================================================================================
+Other
+================================================================================
+
+-The search wont work, because i have no idea how to run Elastica Search
+Engine on remote server, all my tries had failed ;D
+
+-There may occur some problems with mails.
+
+-you can log-in as user1 with password: pass1. Creation of acc may not work
+because of google and their privacy policy.
+
+-be aware of bugs in responsive layout desing, i'll update it in future, 
+maybe:). 
--- src/Project/LineOfWorkBundle/DataFixtures/ORM/JobFixtures.php
+++ src/Project/LineOfWorkBundle/DataFixtures/ORM/JobFixtures.php
@@ -27,7 +27,7 @@ class JobFixtures extends AbstractFixture implements OrderedFixtureInterface{
          $job1->setCompany('Sensio Labs');
          //$job1->setLogo('sensio-labs.gif');
          $job1->setUrl('http://www.sensiolabs.com/');
-         $job1->setPosition('Web Developer');
+         $job1->setPosition('Apple Developer');
          $job1->setLocation('Paris, France');
          $job1->setDescription('You\'ve already developed websites with symfony and you want to work with Open-Source technologies. You have a minimum of 3 years experience in web development with PHP or Java and you wish to participate to development of Web 2.0 sites using the best frameworks available.');
          $job1->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
@@ -44,7 +44,7 @@ class JobFixtures extends AbstractFixture implements OrderedFixtureInterface{
          $job2->setCompany('Extreme Sensio');
          $job2->setLogo('extreme-sensio.gif');
          $job2->setUrl('http://www.extreme-sensio.com/');
-         $job2->setPosition('Web Designer');
+         $job2->setPosition('Desktop Designer');
          $job2->setLocation('Paris, France');
          $job2->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.');
          $job2->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
@@ -53,6 +53,57 @@ class JobFixtures extends AbstractFixture implements OrderedFixtureInterface{
          $job2->setToken('job_2');
          $job2->setEmail('job@example.com');
          $job2->setExpiresAt(new \DateTime('+30 days'));
+         
+         $job3 = new Job();
+         $job3->setCategory($object->merge($this->getReference('category-design')));
+         $job3->setUser($object->merge($this->getReference('user2')));
+         $job3->setType('part-time');
+         $job3->setCompany('Extreme Sensio');
+         $job3->setLogo('extreme-sensio.gif');
+         $job3->setUrl('http://www.extreme-sensio.com/');
+         $job3->setPosition('Android Designer');
+         $job3->setLocation('Paris, France');
+         $job3->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.');
+         $job3->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
+         $job3->setIsPublic(true);
+         $job3->setIsActivated(true);
+         $job3->setToken('job_3');
+         $job3->setEmail('job@example.com');
+         $job3->setExpiresAt(new \DateTime('+30 days'));
+         
+         $job4 = new Job();
+         $job4->setCategory($object->merge($this->getReference('category-programming')));
+         $job4->setUser($object->merge($this->getReference('user1')));
+         $job4->setType('full-time');
+         $job4->setCompany('Sensio Labs');
+         //$job1->setLogo('sensio-labs.gif');
+         $job4->setUrl('http://www.sensiolabs.com/');
+         $job4->setPosition('PHP Developer');
+         $job4->setLocation('Cracow, Poland');
+         $job4->setDescription('You\'ve already developed websites with symfony and you want to work with Open-Source technologies. You have a minimum of 3 years experience in web development with PHP or Java and you wish to participate to development of Web 2.0 sites using the best frameworks available.');
+         $job4->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
+         $job4->setIsPublic(true);
+         $job4->setIsActivated(true);
+         $job4->setToken('job_4');
+         $job4->setEmail('job@example.com');
+         $job4->setExpiresAt(new \DateTime('+30 days'));
+         
+         $job5 = new Job();
+         $job5->setCategory($object->merge($this->getReference('category-programming')));
+         $job5->setUser($object->merge($this->getReference('user1')));
+         $job5->setType('full-time');
+         $job5->setCompany('Sensio Labs');
+         //$job1->setLogo('sensio-labs.gif');
+         $job5->setUrl('http://www.sensiolabs.com/');
+         $job5->setPosition('Ruby Developer');
+         $job5->setLocation('New York, USA');
+         $job5->setDescription('You\'ve already developed websites with symfony and you want to work with Open-Source technologies. You have a minimum of 3 years experience in web development with PHP or Java and you wish to participate to development of Web 2.0 sites using the best frameworks available.');
+         $job5->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
+         $job5->setIsPublic(true);
+         $job5->setIsActivated(true);
+         $job5->setToken('job_5');
+         $job5->setEmail('job@example.com');
+         $job5->setExpiresAt(new \DateTime('+30 days'));
          
          for($i=3 ;$i<30; $i++){
              $job = new Job();
@@ -92,9 +143,12 @@ class JobFixtures extends AbstractFixture implements OrderedFixtureInterface{
  
         $em->persist($job);
     }*/
- 
+         
          $object->persist($job1);
          $object->persist($job2);
+         $object->persist($job3);
+         $object->persist($job4);
+         $object->persist($job5);
          $object->flush();
     }
  
